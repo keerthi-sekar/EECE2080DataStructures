dec37599e07335f9033532ec3633689994488e28
Keerth Sekar <keerthi.sekar101@gmail.com>
2020-09-18 17:28:10 -0400

    Update Towers of Hannoi (Lab 2)

--- Lab02/main.cpp
+++ Lab02/main.cpp
@@ -2,10 +2,10 @@
 //
 
 #include "StackQueue.h"
-#include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
 
 using namespace std;
 
@@ -24,43 +24,39 @@ public:
 	{
 		if (m_stack3.size() == 4)
 		{
-			for (int i = 0; i < m_stack3.size(); i++)
+			if (m_stack3.at(0) == 4 && m_stack3.at(1) == 3 && m_stack3.at(2) == 2 && m_stack3.at(3) == 1)
 			{
-				if (m_stack3[i] > m_stack3[i + 1])
-				{
-					m_GameEnded = false;
-				}
-				else
-				{
-					m_GameEnded = true;
-				}
+				m_GameEnded = true;
+			}
+			else
+			{
+				m_GameEnded = false;
 			}
 		}
 		else
 		{
 			m_GameEnded = false;
 		}
-
 		return m_GameEnded;
 	}
 
 	void PrintTowers()
 	{
-		cout << "Pole 1: ";
+		cout << "Column 1: ";
 		for (int i = 0; i < m_stack1.size(); i++)
 		{
 			cout << m_stack1[i] << " ";
 		}
 		cout << endl;
 
-		cout << "Pole 2: ";
+		cout << "Column 2: ";
 		for (int i = 0; i < m_stack2.size(); i++)
 		{
 			cout << m_stack2[i] << " ";
 		}
 		cout << endl;
 
-		cout << "Pole 3: ";
+		cout << "Column 3: ";
 		for (int i = 0; i < m_stack3.size(); i++)
 		{
 			cout << m_stack3[i] << " ";
@@ -68,13 +64,15 @@ public:
 		cout << endl;
 	}
 
+	//bool fileMoves()
+
 	bool makeMove(int disk, int colFrom, int colTo)
 	{
 		if (disk >= 1 && (colFrom != colTo))
 		{
 			if (colFrom == 1 && m_stack1.back() == disk)
 			{
-				if(m_stack2.empty() && colTo == 2)
+				if (m_stack2.empty() && colTo == 2)
 				{
 					m_stack2.push_back(m_stack1.back());
 					m_stack1.pop_back();
@@ -84,7 +82,7 @@ public:
 					m_stack2.push_back(m_stack1.back());
 					m_stack1.pop_back();
 				}
-				else if(m_stack3.empty() && colTo == 3)
+				else if (m_stack3.empty() && colTo == 3)
 				{
 					m_stack3.push_back(m_stack1.back());
 					m_stack1.pop_back();
@@ -102,7 +100,7 @@ public:
 			}
 			else if (colFrom == 2 && m_stack2.back() == disk)
 			{
-				if(m_stack1.empty() && colTo == 1)
+				if (m_stack1.empty() && colTo == 1)
 				{
 					m_stack1.push_back(m_stack2.back());
 					m_stack2.pop_back();
@@ -112,7 +110,7 @@ public:
 					m_stack1.push_back(m_stack2.back());
 					m_stack2.pop_back();
 				}
-				else if(m_stack3.empty() && colTo == 3)
+				else if (m_stack3.empty() && colTo == 3)
 				{
 					m_stack3.push_back(m_stack2.back());
 					m_stack2.pop_back();
@@ -130,17 +128,17 @@ public:
 			}
 			else if (colFrom == 3 && m_stack3.back() == disk)
 			{
-				if(m_stack1.empty() && colTo == 1)
+				if (m_stack1.empty() && colTo == 1)
 				{
 					m_stack1.push_back(m_stack3.back());
 					m_stack3.pop_back();
 				}
-				else if (colTo == 1 && m_stack1.back() > disk)
+				if (colTo == 1 && m_stack1.back() > disk)
 				{
 					m_stack1.push_back(m_stack3.back());
 					m_stack3.pop_back();
 				}
-				else if(m_stack2.empty() && colTo == 2)
+				else if (m_stack2.empty() && colTo == 2)
 				{
 					m_stack2.push_back(m_stack3.back());
 					m_stack3.pop_back();
@@ -163,7 +161,6 @@ public:
 				cout << "Please check the state of the game." << endl;
 				return false;
 			}
-			IsGameEnded();
 		}
 		else
 		{
@@ -187,99 +184,21 @@ int main()
 	cout << "Progam will exit once towers have  been successfully moved of" << endl;
 	cout << "or string of -1 is entered. Moves must be entired in format of" << endl;
 	cout << "<disk>,<column from>,<column to>   NOTE no spaces!!!!" << endl;
-	cout << "To Begin: Type 1 to interactively play or Type 0 to insert a file for your moves." << endl;
 
 	TowersOfHannoiGame game;
 
 	bool receivedEndToken = false;
-	string manualInput = "0";
+	string gameType = "0";
 
-	cout << "Would you like to play manually or input a file?" << endl;
-	getline(cin, manualInput);
-
-	if (manualInput == "0")
+	cout << "Enter 1 for manual gameplay or 0 for file gameplay." << endl;
+	getline(cin, gameType);
+	if (gameType == "0")
 	{
-		ifstream file;
-		file.open("moves.txt", ios::in);
-		if(file.is_open())
-		{
-			cout << "opened file" << endl;
-		}
+		ifstream file("moves.txt");
 		string inputLine;
-		while (getline(file, inputLine) || game.IsGameEnded() || !receivedEndToken)
+		while (getline(file, inputLine) || game.IsGameEnded())
 		{
-			cout << inputLine << endl;
 			game.PrintTowers();
-
-			if (inputLine == "-1")
-			{
-				receivedEndToken = true;
-			}
-			else
-			{
-				std::vector<std::string> output;
-				std::string::size_type prev_pos = 0, pos = 0;
-				// Snippet from https://stackoverflow.com/questions/5167625/splitting-a-c-stdstring-using-tokens-e-g
-				// tokenizing a string
-				while ((pos = inputLine.find(",", pos)) != std::string::npos)
-				{
-					std::string substring(inputLine.substr(prev_pos, pos - prev_pos));
-					output.push_back(substring);
-					prev_pos = ++pos;
-				}
-				//Need to get last token
-				output.push_back(inputLine.substr(prev_pos, pos - prev_pos)); // Last word
-
-				if (output.size() != 3)
-				{
-					receivedEndToken = true;
-					cout << "(1) Invalid input recieved = " + inputLine << endl;
-				}
-				else
-				{
-					std::string disk = output[0];
-					std::string from = output[1];
-					std::string to = output[2];
-
-					if (disk.size() == 0 || from.size() == 0 || to.size() == 0)
-					{
-						receivedEndToken = true;
-						cout << "(2) Invalid input recieved = " + inputLine << endl;
-					}
-
-					int diskId = atoi(disk.c_str());
-					int fromId = atoi(from.c_str());
-					int toId = atoi(to.c_str());
-					//if any number is zero we didn't have a integer
-					if (diskId == 0 || fromId == 0 || toId == 0)
-					{
-						receivedEndToken = true;
-						cout << "(3) Invalid input recieved = " + inputLine << endl;
-					}
-					cout << "Disk " << diskId << " From " << fromId << " To " << toId << endl;
-					game.makeMove(diskId, fromId, toId);
-				}
-			}
-		}
-	}
-	else if (manualInput == "1")
-	{
-		while (!receivedEndToken || game.IsGameEnded())
-		{
-			string inputLine;
-			cout << "Enter Move " << endl;
-
-			if(game.IsGameEnded())
-			{
-				inputLine == "-1";
-			}
-			else
-			{
-				getline(cin, inputLine);
-			}
-
-			game.PrintTowers();
-
 			if (inputLine == "-1")
 			{
 				receivedEndToken = true;
@@ -332,6 +251,69 @@ int main()
 			}
 		}
 	}
+	else if (gameType == "1")
+	{
+		while (!receivedEndToken)
+		{
+			std::string inputLine;
+			game.PrintTowers();
+			cout << "Enter Move " << endl;
+			getline(cin, inputLine);
+			if (inputLine == "-1")
+			{
+				receivedEndToken = true;
+			}
+			else
+			{
+				std::vector<std::string> output;
+				std::string::size_type prev_pos = 0, pos = 0;
+				// Snippet from https://stackoverflow.com/questions/5167625/splitting-a-c-stdstring-using-tokens-e-g
+				// tokenizing a string
+				while ((pos = inputLine.find(",", pos)) != std::string::npos)
+				{
+					std::string substring(inputLine.substr(prev_pos, pos - prev_pos));
+					output.push_back(substring);
+					prev_pos = ++pos;
+				}
+				//Need to get last token
+				output.push_back(inputLine.substr(prev_pos, pos - prev_pos)); // Last word
 
+				if (output.size() != 3)
+				{
+					receivedEndToken = true;
+					cout << "Invalid input recieved = " + inputLine << endl;
+				}
+				else
+				{
+					std::string disk = output[0];
+					std::string from = output[1];
+					std::string to = output[2];
+
+					if (disk.size() == 0 || from.size() == 0 || to.size() == 0)
+					{
+						receivedEndToken = true;
+						cout << "Invalid input recieved = " + inputLine << endl;
+					}
+
+					int diskId = atoi(disk.c_str());
+					int fromId = atoi(from.c_str());
+					int toId = atoi(to.c_str());
+					//if any number is zero we didn't have a integer
+					if (diskId == 0 || fromId == 0 || toId == 0)
+					{
+						receivedEndToken = true;
+						cout << "Invalid input recieved = " + inputLine << endl;
+					}
+
+					cout << "Disk " << diskId << " From " << fromId << " To " << toId << endl;
+					game.makeMove(diskId, fromId, toId);
+					if (game.IsGameEnded() == true)
+					{
+						receivedEndToken = true;
+					}
+				}
+			}
+		}
+	}
 	return 0;
 }
