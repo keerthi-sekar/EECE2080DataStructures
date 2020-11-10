c63ad22d26025f93e181441e92699dfd489a2076
Keerth Sekar <keerthi.sekar101@gmail.com>
2020-11-10 13:39:16 -0500

    Create ArrayBasedQueue

--- Lab08/PriorityQueue.cpp
+++ Lab08/PriorityQueue.cpp
@@ -1,9 +1,51 @@
 #include "PriorityQueue.h"
 #include <iostream>
 #include <string>
-#include <cmath>
-#include <iomanip>
-#include <sstream>
 
 using namespace std;
 
+int MVALUES_SIZE = 10;
+
+ArrayBasedQueue::ArrayBasedQueue()
+{
+    for(int i = 0; i < MVALUES_SIZE; i++)
+    {
+        m_values[i] = -1;
+    }
+}
+
+bool ArrayBasedQueue::Insert(int value)
+{
+    for(int i = 0; i < MVALUES_SIZE; i++)
+    {
+        if(m_values[i] == -1)
+        {
+            m_values[i] = value;
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool ArrayBasedQueue::Remove(int value)
+{
+    for(int i = 0; i < MVALUES_SIZE; i++)
+    {
+        if(m_values[i] == value)
+        {
+            m_values[i] = -1;
+            return true;
+        }
+    }
+
+    return false;
+}
+
+void:: ArrayBasedQueue::Print()
+{
+    for(int i = 0; i < MVALUES_SIZE; i++)
+    {
+        cout << m_values[i] << endl;
+    }
+}
---
+++ Lab08/PriorityQueue.h
@@ -0,0 +1,16 @@
+
+class ArrayBasedQueue
+{
+    public:
+        ArrayBasedQueue();
+        void Insert(int value);
+        void Remove(int value);
+        void Print();
+    private:
+        int m_values[10];
+};
+
+class Heap
+{
+
+};
